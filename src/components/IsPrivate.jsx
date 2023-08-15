6c9245ebaeb6780cf2dd750694e84f19f86b2cf3
Charlotte Forsdick <c.l.forsdick@gmail.com>
2023-08-15 21:42:44 +0200

    login redirect working

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,18 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/auth.context";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function IsPrivate({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (!isLoggedIn) {
+  //     console.log("it works");
+  //   }
+  // }, [isLoggedIn]);
+  console.log(isLoggedIn, "is logged in");
 
   // If the authentication is still loading ⏳
   if (isLoading) return <p>Loading ...</p>;
 
   if (!isLoggedIn) {
-  // If the user is not logged in ❌
-    return <Navigate to="/login" />;
+    // If the user is not logged in ❌
+    return navigate("/login");
   } else {
-  // If the user is logged in, allow to see the page ✅
+    // If the user is logged in, allow to see the page ✅
     return children;
   }
 }
